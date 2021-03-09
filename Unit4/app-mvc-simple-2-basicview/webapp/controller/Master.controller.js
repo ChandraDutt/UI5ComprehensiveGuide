94c0a71708fc2268b03dd4d8337fc393d382fb82
Chandra Dutt <chandradutt.chandu@gmail.com>
2021-03-09 17:53:36 +0000

    Basic view in js, master.view.js created

---
+++ Unit4/app-mvc-simple-2-basicview/webapp/controller/Master.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+"sap/ui/core/mvc/Controller"
+], function(Controller) {
+"use strict";
+return Controller.extend("com.chandu.ui5.appmvcsimple2basicview.controller.Master", {
+});
+});
---
+++ Unit4/app-mvc-simple-2-basicview/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comchandu.ui5./app-mvc-simple-2-basicview/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
