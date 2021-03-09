2831923ba0e17c187f619436f44c30c7aa496547
Chandra Dutt <chandradutt.chandu@gmail.com>
2021-03-09 18:50:10 +0000

    Basic MVC with single page view and controller

--- Unit4/app-mvc-simple-2-basicview/webapp/controller/Master.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define([
-"sap/ui/core/mvc/Controller"
-], function(Controller) {
-"use strict";
-return Controller.extend("com.chandu.ui5.appmvcsimple2basicview.controller.Master", {
-});
-});
