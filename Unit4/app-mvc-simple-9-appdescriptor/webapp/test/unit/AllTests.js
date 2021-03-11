41f9b81624a6028d42ea85baaa080fb54ba45fe0
Chandra Dutt <chandradutt.chandu@gmail.com>
2021-03-11 19:35:47 +0000

    Unit4 App descriptor is added to project

---
+++ Unit4/app-mvc-simple-9-appdescriptor/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sapui5demo.mvcapp./app-mvc-simple-9-appdescriptor/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
---
+++ Unit4/app-mvc-simple-9-appdescriptor/webapp/test/unit/controller/View1.controller.js
@@ -0,0 +1,16 @@
+/*global QUnit*/
+
+sap.ui.define([
+	"sapui5demo.mvcapp./app-mvc-simple-9-appdescriptor/controller/View1.controller"
+], function (Controller) {
+	"use strict";
+
+	QUnit.module("View1 Controller");
+
+	QUnit.test("I should test the View1 controller", function (assert) {
+		var oAppController = new Controller();
+		oAppController.onInit();
+		assert.ok(oAppController);
+	});
+
+});
